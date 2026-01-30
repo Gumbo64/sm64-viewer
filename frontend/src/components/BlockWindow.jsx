b01a320ff48f2cbe3702293160caffde950a36ba
Gumbo64 <35547211+Gumbo64@users.noreply.github.com>
2026-01-31 01:06:58 +1100

    starting index allowed

--- frontend/src/components/BlockWindow.jsx
+++ frontend/src/components/BlockWindow.jsx
@@ -8,7 +8,7 @@ function BlockWindow({ solution }) {
     const canvasRef = useRef(null);
     const [playing, setPlaying] = useState(false);
     const [speed, setSpeed] = useState(1); // <-- new state
-
+    const [splitIndex, setSplitIndex] = useState(0); // <-- Added state for splitting
     if (solution == null) {
         return <h1>Null solution</h1>;
     }
@@ -17,15 +17,32 @@ function BlockWindow({ solution }) {
         if (!playing) {
             let seed = NaN;
             let game_config = new GameConfig(NaN, null);
+            console.log(solution.length)
             setPlaying(true);
+
+            const firstPart = solution.slice(0, splitIndex);
+            const secondPart = solution.slice(splitIndex);
+
+            let [engine, _] = await sm64_playback(
+                canvasRef.current,
+                firstPart,
+                false,
+                seed,
+                game_config,
+                200,
+                200
+            );
             await sm64_playback(
                 canvasRef.current,
-                solution,
+                secondPart,
                 false,
                 seed,
                 game_config,
                 speed,
-                speed
+                speed,
+                async () => false,
+                () => false,
+                engine
             );
             setPlaying(false);
         }
@@ -35,7 +52,16 @@ function BlockWindow({ solution }) {
         <div>
             <ul>
                 <li>Solution length: {solution.length}</li>
-
+                <li>
+                    Start from index (wait for it to load):&nbsp;
+                    <input
+                        type="number"
+                        value={splitIndex}
+                        min={0}
+                        max={solution.length}
+                        onChange={(e) => setSplitIndex(Number(e.target.value))}
+                    />
+                </li>
                 <li>
                     Speed (1x speed to unmute):&nbsp;
                     <input
